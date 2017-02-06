a70f54f2b1a8dd1d5ee43525694fefda47a4db05
Konrad <konrjas@gmail.com>
2017-02-06 20:42:55 +0100

    Packages

---
+++ BatimetryApp/Services/AwsResourceLocator.cs
@@ -0,0 +1,12 @@
+﻿using BatimetryApp.Services.Interfaces;
+
+namespace BatimetryApp.Services
+{
+	public class AwsResourceLocator : IAwsResourceLocator
+	{
+		public string CreateResourcePath()
+		{
+			return string.Empty;
+		}
+	}
+}
---
+++ BatimetryApp/Services/Interfaces/IAwsResourceLocator.cs
@@ -0,0 +1,7 @@
+﻿namespace BatimetryApp.Services.Interfaces
+{
+	public interface IAwsResourceLocator
+	{
+		string CreateResourcePath();
+	}
+}
